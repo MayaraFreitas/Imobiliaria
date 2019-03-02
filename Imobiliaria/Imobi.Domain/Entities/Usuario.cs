a28f1a10317ea6297e9c68eaf444290b706d0221
Mayara <mah.alvesfreitas@gmail.com>
2019-03-02 12:09:21 -0300

    alterando formato da construção do projeto

---
+++ Imobiliaria/Imobi.Domain/Entities/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using Imobi.Domain.VOs;
+using System;
+
+namespace Imobi.Domain.Entities
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public Nome Nome { get; set; }
+        public string Apelido { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public byte Status { get; set; }
+    }
+}
