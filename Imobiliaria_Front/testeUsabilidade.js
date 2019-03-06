97c9cd4ced9ccc1b63ad61c06be700249d8ecbf3
RodrigoDisselli <disselli.rodrigo@gmail.com>
2019-03-06 11:27:24 -0300

    ajuste_url no arquivo de teste

--- Imobiliaria_Front/testeUsabilidade.js
+++ Imobiliaria_Front/testeUsabilidade.js
@@ -1,10 +1,10 @@
 var classname = document.getElementsByClassName("card");
 
 var card = function() {
-    if(window.location.href == "file:///C:/Users/Rodrigo_Almeida/Documents/Faculdade/Front_2.0/listaVistorias.html#!"){
+    if(window.location.href.indexOf("listaVistorias.html") > -1){
         window.location.href = "listaComodos.html";
     }
-    if(window.location.href == "file:///C:/Users/Rodrigo_Almeida/Documents/Faculdade/Front_2.0/listaComodos.html"){
+    if(window.location.href.indexOf("listaComodos.html") > -1){
         window.location.href = "listaNotas.html";
     }
     
