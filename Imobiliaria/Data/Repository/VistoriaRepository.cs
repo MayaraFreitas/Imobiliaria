d379cb6910cc14a121653b37917194a36565390e
MayaraFreitas <mah.alvesfreitas@hotmail.com>
2019-04-22 18:49:14 -0300

    criando Resources

--- Imobiliaria/Data/Repository/VistoriaRepository.cs
+++ Imobiliaria/Data/Repository/VistoriaRepository.cs
@@ -11,19 +11,30 @@ namespace Imobiliaria.Data.Repository
     public interface IVistoriaRepository
     {
         void InserirSolicitacao(SolicitacaoVO solicitacao);
+        SolicitacaoVO BuscarSolicitacao(SolicitacaoFiltroVO filtro);
+        SolicitacaoVO BuscarSolicitacaoDetalhe(int codigoSolicitacao);
+
+        // teste
         IList<SolicitacaoVO> FindAll();
     }
     public class VistoriaRepository : IVistoriaRepository
     {
+        #region Constantes
+
         private readonly ApplicationDbContext _context;
         private readonly IMapper _mapper;
 
+        #endregion
+
+        #region Construtor
         public VistoriaRepository(ApplicationDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
+        #endregion
 
+        #region Solicitação
         public void InserirSolicitacao(SolicitacaoVO solicitacaoVO)
         {
             Solicitacao solicitacao = _mapper.Map<Solicitacao>(solicitacaoVO);
@@ -31,10 +42,25 @@ namespace Imobiliaria.Data.Repository
             _context.SaveChanges();
         }
 
+        public SolicitacaoVO BuscarSolicitacao(SolicitacaoFiltroVO filtro)
+        {
+            return null;
+        }
+
+        public SolicitacaoVO BuscarSolicitacaoDetalhe(int codigoSolicitacao)
+        {
+            Solicitacao solicitacao = _context.Solicitacao.Find(codigoSolicitacao);
+            return _mapper.Map<SolicitacaoVO>(solicitacao);
+        }
+
+        #endregion
+
+        #region Metodos de Teste
         public IList<SolicitacaoVO> FindAll()
         {
             IList<Solicitacao> lstSolicitacao = _context.Solicitacao.ToList();
             return _mapper.Map<IList<SolicitacaoVO>>(lstSolicitacao);
         }
+        #endregion
     }
 }
